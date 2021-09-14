291544f29809b6ee8fa2874c4dd86c4523677e80
David Vargas <dvargas92495@gmail.com>
2021-09-14 18:29:30 -0400

    Pages support a render body first prop

--- src/_html.tsx
+++ src/_html.tsx
@@ -9,18 +9,38 @@ import(`./${pagePath}`)
   .then((r) => {
     const Page = r.default;
     const Head = r.Head || React.Fragment;
-    fs.writeFileSync(
-      path.join("out", pagePath.replace(/\.js$/i, ".html")),
-      ReactDOMServer.renderToString(
-        <html>
-          <head>
-            <Head />
-          </head>
-          <body>
-            <Page />
-          </body>
-        </html>
-      )
-    );
+    const renderBodyFirst = !r.renderBodyFirst;
+    const outfile = path.join("out", pagePath.replace(/\.js$/i, ".html"));
+    if (renderBodyFirst) {
+      const body = ReactDOMServer.renderToString(<Page />);
+      const head = ReactDOMServer.renderToString(<Head />);
+      fs.writeFileSync(
+        outfile,
+        `<!DOCTYPE html>
+<html>
+  <head>
+    ${head}
+  </head>
+  <body>
+    ${body}
+  </body>
+</html>
+`
+      );
+    } else {
+      fs.writeFileSync(
+        outfile,
+        ReactDOMServer.renderToString(
+          <html>
+            <head>
+              <Head />
+            </head>
+            <body>
+              <Page />
+            </body>
+          </html>
+        )
+      );
+    }
   })
   .catch((e) => console.error(e.message));
