4e36cfcf3ace1f672c6f51106ec856a0dc0c202a
David Vargas <dvargas92495@gmail.com>
2021-10-07 15:08:44 -0400

    fix naming in index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { default as build } from "./build";
 export { default as deploy } from "./deploy";
 export { default as fe } from "./fe";
 export { default as migrate } from "./migrate";
-export { default as buildApi } from "./build-api";
-export { default as deployApi } from "./deploy-api";
+export { default as compile } from "./compile";
+export { default as publish } from "./publish";
 export { default as api } from "./api";
