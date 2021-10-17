580dba2d6c246883ccd5d20a04cc7349476d36d2
David Vargas <dvargas92495@gmail.com>
2021-10-17 17:15:03 -0400

    handle compile no functions dir

--- src/compile.ts
+++ src/compile.ts
@@ -1,30 +1,36 @@
+import fs from "fs";
 import { build as esbuild } from "esbuild";
 import { prepareApiBuild, readDir } from "./common";
 
 const commonRegex = /^functions[/\\]_common/;
 
 const compile = (): Promise<number> =>
-  prepareApiBuild()
-    .then((opts) =>
-      esbuild({
-        ...opts,
-        entryPoints: Object.fromEntries(
-          readDir("functions")
-            .filter((f) => !commonRegex.test(f))
-            .map((f) => [
-              f.replace(/\.[t|j]s$/, "").replace(/^functions[/\\]/, ""),
-              `./${f}`,
-            ])
-        ),
-        minify: true,
-      })
-    )
-    .then((r) => {
-      if (r.errors.length) {
-        throw new Error(JSON.stringify(r.errors));
-      } else {
-        return r.errors.length;
-      }
-    });
+  fs.existsSync("functions")
+    ? prepareApiBuild()
+        .then((opts) =>
+          esbuild({
+            ...opts,
+            entryPoints: Object.fromEntries(
+              readDir("functions")
+                .filter((f) => !commonRegex.test(f))
+                .map((f) => [
+                  f.replace(/\.[t|j]s$/, "").replace(/^functions[/\\]/, ""),
+                  `./${f}`,
+                ])
+            ),
+            minify: true,
+          })
+        )
+        .then((r) => {
+          if (r.errors.length) {
+            throw new Error(JSON.stringify(r.errors));
+          } else {
+            return r.errors.length;
+          }
+        })
+    : Promise.resolve().then(() => {
+        console.log("No `functions` directory to compile. Exiting...");
+        return 0;
+      });
 
 export default compile;
