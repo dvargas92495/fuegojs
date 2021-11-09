0b3357a632d2e55b7ae5dc217b85cd668ad07480
David Vargas <dvargas92495@gmail.com>
2021-11-08 22:58:53 -0500

    0.16.1

--- src/build.ts
+++ src/build.ts
@@ -41,8 +41,7 @@ const getEntryPoints = (paths: string[]) => {
         params: result?.regex.exec(path)?.groups || {},
       }));
   }
-  return pages
-    .map((entry) => ({ entry, params: {} }));
+  return pages.map((entry) => ({ entry, params: {} }));
 };
 
 const buildDir = ({ path = "" }: BuildArgs): Promise<number> => {
