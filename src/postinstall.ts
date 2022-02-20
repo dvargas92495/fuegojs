54f1e4c0fa8d4a5597d78dfc6b663d4fb4cdc553
David Vargas <dvargas92495@gmail.com>
2022-02-19 19:00:39 -0700

    0.22.11

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -76,9 +76,12 @@ const postinstall = (modulesToTranspile: string[]): Promise<number> => {
       return setupRemix(SetupPlatform.Node);
     })
     .then(() => {
-      if (fs.existsSync('./node_modules/@remix-run/react/node_modules') && fs.existsSync('./node_modules/react-router-dom')) {
+      if (
+        fs.existsSync("./node_modules/@remix-run/react/node_modules") &&
+        fs.existsSync("./node_modules/react-router-dom")
+      ) {
         // need to have one version of react router - not sure why this is happening
-        fs.rmdirSync('./node_modules/@remix-run/react/node_modules');
+        fs.rmdirSync("./node_modules/@remix-run/react/node_modules");
       }
     })
     .then(() => console.log("done!"))
