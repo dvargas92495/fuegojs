f62f59b207b084abd499fb2b5e8d4e1430cdc373
David Vargas <dvargas92495@gmail.com>
2022-09-15 17:48:26 -0400

    add src index warning

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-export { default as init } from "./commands/init";
-export { default as build } from "./build";
-export { default as deploy } from "./deploy";
-export { default as dev } from "./dev";
-export { default as compile } from "./commands/compile";
-export { default as publish } from "./publish";
-export { default as api } from "./api";
-export { default as migrate } from "./internal/migrate";
+throw new Error(
+  `Do not import from fuegojs directly. Instead import the *method* you need directly. For example: 
+
+import mysql from "fuegojs/utils/mysql;"`
+);
