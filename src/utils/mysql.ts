aed7a9a3285add8228d1e9ebc6f1663266eeb8dc
David Vargas <dvargas92495@gmail.com>
2022-10-31 17:32:13 -0400

    Enter database URL directly to connection

--- src/utils/mysql.ts
+++ src/utils/mysql.ts
@@ -4,18 +4,8 @@ import { v4 } from "uuid";
 const connectionMap: Record<string, mysql.Connection> = {};
 
 const createConnection = (id = v4()) => {
-  const DATABASE_URL_REGEX =
-    /^mysql:\/\/([a-z0-9_]+):([^@]{4,32})@([a-z0-9.-]+):(\d{3,5})\/([a-z_]+)$/;
-  const matches = DATABASE_URL_REGEX.exec(process.env.DATABASE_URL || "");
-  if (!matches) return Promise.reject(`Error parsing Database URL`);
   return mysql
-    .createConnection({
-      host: matches[3],
-      user: matches[1],
-      port: Number(matches[4]),
-      database: matches[5],
-      password: matches[2],
-    })
+    .createConnection(process.env.DATABASE_URL || "")
     .then((con) => (connectionMap[id] = con));
 };
 
