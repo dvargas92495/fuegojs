618b69da3ad721d86ceb45a2d351bf6f40467d9d
David Vargas <dvargas92495@gmail.com>
2022-11-27 23:41:21 -0500

    hacking plan to not wait for terraform

--- src/commands/plan.ts
+++ src/commands/plan.ts
@@ -1,6 +1,29 @@
 import child_process from "child_process";
+import fs from "fs";
 
 const plan = async ({ sql }: { sql?: boolean }): Promise<number> => {
+  // This is a hack until https://github.com/cdktf/cdktf-provider-aws bumps up to 4.41.0
+  if (fs.existsSync("./node_modules/@cdktf/provider-aws/lib")) {
+    fs.readdirSync("./node_modules/@cdktf/provider-aws/lib").forEach((dir) => {
+      const index = `./node_modules/@cdktf/provider-aws/lib/${dir}/index.js`;
+      console.log(dir, index);
+      if (
+        fs.existsSync(`./node_modules/@cdktf/provider-aws/lib/${dir}/index.js`)
+      ) {
+        fs.writeFileSync(
+          index,
+          fs
+            .readFileSync(index)
+            .toString()
+            .replace(
+              /providerVersion: '4\.39\.0',/,
+              `providerVersion: '4.41.0',`
+            )
+        );
+      }
+    });
+  }
+
   if (sql) {
     child_process.execSync(`npx ts-node-esm data/main.ts`, {
       stdio: "inherit",
